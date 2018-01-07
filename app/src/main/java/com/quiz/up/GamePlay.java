dc3d20203551a9eb1b03590ce2eb56ec560d619f
Gautam <gautamsingh1997@gmail.com>
2018-01-07 18:48:14 +0530

    Game Play ui improved
    added question lyout, option layout and some touch effects

--- app/src/main/java/com/quiz/up/GamePlay.java
+++ app/src/main/java/com/quiz/up/GamePlay.java
@@ -3,6 +3,8 @@ package com.quiz.up;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,13 +13,18 @@ import java.sql.SQLOutput;
 
 public class GamePlay extends AppCompatActivity {
         JSONObject Questions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Applying immersive mode
         View dv=getWindow().getDecorView();
         int ui=View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
         dv.setSystemUiVisibility(ui);
+
         setContentView(R.layout.activity_game_play);
+
 
         try
         {
@@ -29,7 +36,6 @@ public class GamePlay extends AppCompatActivity {
         }
         
 
-
-
+      
     }
 }

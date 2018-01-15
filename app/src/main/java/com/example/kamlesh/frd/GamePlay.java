ef2c0d1ff1d20174544babb5e3e8ebc1bc98badd
Gautam <gautamsingh1997@gmail.com>
2018-01-15 13:21:27 +0530

    Solved an imersive mode issue in GamePlay.java

--- app/src/main/java/com/example/kamlesh/frd/GamePlay.java
+++ app/src/main/java/com/example/kamlesh/frd/GamePlay.java
@@ -49,6 +49,21 @@ public class GamePlay extends AppCompatActivity implements View.OnClickListener 
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView= getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 

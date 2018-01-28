6a4ba5fb93689bd4f222de8e6265ed22d1f0a212
kamlesh sahu <vasubantikamlesh@gmail.com>
2018-01-28 23:24:14 +0530

    fb,gsign button onclk listener fn(login page)

--- app/src/main/java/com/example/kamlesh/frd/LoginActivity.java
+++ app/src/main/java/com/example/kamlesh/frd/LoginActivity.java
@@ -1,11 +1,48 @@
 package com.example.kamlesh.frd;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.Set;
 
 import me.relex.circleindicator.CircleIndicator;
 
@@ -14,15 +51,15 @@ public class LoginActivity extends AppCompatActivity {
 
     LinearLayout facebookLogin, googleLogin, guestLogin;
 
-    /*public FirebaseAuth mAuth;
+    public FirebaseAuth mAuth;
     public CallbackManager mCallbackManager;
     FirebaseAuth.AuthStateListener mAuthListener;
     final static int RC_SIGN_IN = 2;
     static GoogleApiClient mGoogleSignInClient;
     SharedPreferences prefs;
     ProgressDialog dialog;
-    */
-   /*
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -30,7 +67,7 @@ public class LoginActivity extends AppCompatActivity {
         mAuth.addAuthStateListener(mAuthListener);
 
     }
-*/
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -59,8 +96,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View dv=getWindow().getDecorView();
-        int ui= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        View dv = getWindow().getDecorView();
+        int ui = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
         dv.setSystemUiVisibility(ui);
 
         setContentView(R.layout.activity_login);
@@ -72,45 +109,16 @@ public class LoginActivity extends AppCompatActivity {
         indicator.setViewPager(viewPager);
         viewPager.setOffscreenPageLimit(2);
 
-        facebookLogin = (LinearLayout)findViewById(R.id.facebookLogin);
-        googleLogin = (LinearLayout)findViewById(R.id.googleLogin);
-        guestLogin = (LinearLayout)findViewById(R.id.guestLogin);
+        facebookLogin = (LinearLayout) findViewById(R.id.facebookLogin);
+        googleLogin = (LinearLayout) findViewById(R.id.googleLogin);
+        guestLogin = (LinearLayout) findViewById(R.id.guestLogin);
 
-        facebookLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-            }
-        });
+        dialog = new ProgressDialog(LoginActivity.this);
 
-        googleLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        guestLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
-            }
-        });
-
-
-        /*prefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-        dialog=new ProgressDialog(LoginActivity.this);
-        SignInButton signInButton = findViewById(R.id.signin_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
         mAuth = FirebaseAuth.getInstance();
 
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -133,7 +141,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (firebaseAuth.getCurrentUser() != null) {
-                      FirebaseUser user=firebaseAuth.getCurrentUser();
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
                     System.out.println("oth state change");
                     if (prefs.getBoolean("Islogin", false)) {
                         startActivity(new Intent(LoginActivity.this, Select_Topic.class));
@@ -164,7 +172,8 @@ public class LoginActivity extends AppCompatActivity {
 
 
         mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = findViewById(R.id.login_button);
+        final LoginButton loginButton = findViewById(R.id.login_button);
+
         loginButton.setReadPermissions("email", "public_profile");
         loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -177,14 +186,12 @@ public class LoginActivity extends AppCompatActivity {
                 Set<String> setIterable = loginResult.getAccessToken().getPermissions();
                 Iterable iterable = setIterable;
                 Iterator iterator = iterable.iterator();
-                while (iterator.hasNext()) {
-                    System.out.println(" granted : " + iterator.next().toString());
+                System.out.println(" granted : " + iterator.next().toString());
 
-                }
-*/
-    }
+            }
 
-            /*@Override
+
+            @Override
             public void onCancel() {
                 Log.d("TAG", "facebook:onCancel");
                 // ...
@@ -194,6 +201,28 @@ public class LoginActivity extends AppCompatActivity {
             public void onError(FacebookException error) {
                 Log.d("TAG", "facebook:onError", error);
                 // ...
+            }
+        });
+
+        facebookLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 loginButton.performClick();
+            }
+        });
+
+        googleLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        guestLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "coming soon",
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -317,8 +346,8 @@ public class LoginActivity extends AppCompatActivity {
                 urlConnection = (HttpURLConnection) url.openConnection();
 
                 urlConnection.setRequestMethod("GET");
-                urlConnection.setReadTimeout(10000 *//* milliseconds *//*);
-                urlConnection.setConnectTimeout(15000 *//* milliseconds *//*);
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
 
                 urlConnection.setDoOutput(true);
 
@@ -362,7 +391,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, Select_Topic.class));
                 finish();
             }else{
-                Toast.makeText(LoginActivity.this,"error pls retry",Toast.LENGTH_LONG);
+                Toast.makeText(LoginActivity.this,"error pls retry", Toast.LENGTH_LONG);
                 System.out.println("player register error pls retry....");
             }
         }
@@ -375,6 +404,5 @@ public class LoginActivity extends AppCompatActivity {
             a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(a);
         }
-*/
 
 }
